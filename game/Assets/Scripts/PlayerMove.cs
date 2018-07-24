3475f2237ad59303ab024a3caac7152f792271e6
jjk23 <dlwogh2009@naver.com>
2018-07-24 10:48:36 +0900

    Sheet

---
+++ game/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+    public float MovePower = 1f;
+    public float JumpPower = 5f;
+
+    Rigidbody2D RB;
+
+    Vector3 movement;
+    bool isJumping;
+
+	// Use this for initialization
+	void Start () {
+        RB = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if((Input.GetKeyDown(KeyCode.UpArrow)||Input.GetKeyDown(KeyCode.W))&&!isJumping)
+        {
+            Jump();
+        }
+	}
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 moveVelocity = Vector3.zero;
+
+        if(Input.GetAxis("Horizontal") < 0)
+        {
+            moveVelocity = Vector3.left;
+            gameObject.transform.localScale = new Vector3(-1, 1, 1);
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            moveVelocity = Vector3.right;
+            gameObject.transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        transform.position += moveVelocity * MovePower * Time.deltaTime;
+    }
+    void Jump()
+    {
+        if (isJumping)
+            return;
+
+        RB.velocity = Vector2.zero;
+
+        Vector2 jumpVelocity = new Vector2(0, JumpPower);
+        RB.AddForce(jumpVelocity, ForceMode2D.Impulse);
+
+        isJumping = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "ground")
+        {
+            isJumping = false;
+        }
+        if(collision.gameObject.tag == "Trap")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ game/Assets/Scripts/Springcamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Springcamera : MonoBehaviour {
+    Vector3 yunvec;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        yunvec = GameObject.Find("Yunjae").transform.position;
+        transform.position = Vector3.Lerp(transform.position, yunvec +new Vector3(0,0,-10),0.04f);
+	}
+}
